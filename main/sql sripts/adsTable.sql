e6d299645d0778fa2d8e0c72cd34f82145d97c76
Vyacheslav <82397059+mikki1391@users.noreply.github.com>
2024-06-17 11:23:52 +0300

    Delete main directory

--- main/sql sripts/adsTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `ads` (
-    `id` INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(100) NOT NULL,
-    `description` TEXT NOT NULL,
-    `user_id` INT(11) UNSIGNED NOT NULL,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-);
