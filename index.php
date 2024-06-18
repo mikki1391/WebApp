1bdf5dda53c9854b21d950e44e954cc3f7a829c3
Vyacheslav <82397059+mikki1391@users.noreply.github.com>
2024-06-18 13:07:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Include the necessary files
+require_once 'includes/functions.php';
+require_once 'templates/header.php';
+?>
+
+<main>
+    <div class="container">
+        <h1>Добро пожаловать на сайт объявлений</h1>
+        <p>Пожалуйста <a href="register.php"style="color: blue;">зарегестрируйтесь</a> или пройдите форму <a href="login.php"style="color: blue;">авторизации</a> что бы увидеть объявления</p>
+        <p>Посетите страницу <a href="ads.php" style="color: blue;">все объявления</a> что бы увидеть объявления</p>
+    </div>
+</main>
+
+<?php require_once 'templates/footer.php'; ?>
